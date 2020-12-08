25f2cbfef721b0ff32244cceb0a3f5a79024c5ad
Kazumi Inada <hello@nandenjin.com>
2020-12-08 11:05:38 +0900

    chore: Setup style loaders

--- front/webpack.config.js
+++ front/webpack.config.js
@@ -23,6 +23,10 @@ module.exports = {
         },
       },
       {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
         test: /\.vue$/,
         loader: 'vue-loader',
       },
