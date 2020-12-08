7633576ecf6db3912120a565696fe97754bca902
Kazumi Inada <hello@nandenjin.com>
2020-12-08 11:06:05 +0900

    feat: Implement board UI

--- front/src/Player.vue
+++ front/src/Player.vue
@@ -1,10 +1,15 @@
-<template><div>Player</div></template>
+<template>
+  <div class="board" :style="{ gridTemplateColumns: `repeat(${width}, 1fr)` }">
+    <span class="cell" v-for="i of width * height" :key="i"></span>
+  </div>
+</template>
 
 <script lang="ts">
 import { defineComponent, watch } from 'vue'
 import { useRoute } from 'vue-router'
 import { useStore } from 'vuex'
 import { key, SET_GAME, State } from './store'
+import { BOARD_H, BOARD_W } from './config'
 
 export default defineComponent({
   setup() {
@@ -19,7 +24,27 @@ export default defineComponent({
 
     watch(route, () => loadFromRoute())
 
-    return {}
+    return {
+      width: BOARD_W,
+      height: BOARD_H,
+    }
   },
 })
 </script>
+
+<style lang="scss">
+.board {
+  display: grid;
+  width: 300px;
+
+  .cell {
+    background-color: #eee;
+    margin: 1px;
+    &::before {
+      content: '';
+      display: block;
+      margin-bottom: 100%;
+    }
+  }
+}
+</style>
---
+++ front/src/config.ts
@@ -0,0 +1,2 @@
+export const BOARD_W = 8
+export const BOARD_H = 8
