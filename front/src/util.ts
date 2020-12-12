27a7d570bbe6324a865fade8ab6a8ce68a8dcdef
Kazumi Inada <hello@nandenjin.com>
2020-12-13 00:56:20 +0900

    refactor: Use bit operator for util functions

--- front/src/util.ts
+++ front/src/util.ts
@@ -4,16 +4,15 @@ export const isEmpty = (n: number[]): boolean => n.every(p => p === 0)
 export const isEqual = (a: number[], b: number[]): boolean =>
   ntos(a) === ntos(b)
 export const getBit = (n: number[], i: number): boolean =>
-  ((n[Math.floor(i / 32)] || 0) >>> i % 32) % 2 === 1
+  !!(((n[i >>> 5] || 0) >>> (i & 31)) & 1)
 export const setBit = (
   n: number[],
   i: number,
   v: boolean | number
 ): number[] => {
   const nn = n.map(p => p)
-  nn[Math.floor(i / 32)] =
-    (n[Math.floor(i / 32)] || 0) & (0xffffffff - (0b1 << i % 32))
-  nn[Math.floor(i / 32)] += (v ? 0b1 : 0b0) << i % 32
+  nn[i >>> 5] = (n[i >>> 5] || 0) & (0xffffffff - (0b1 << (i & 31)))
+  nn[i >>> 5] += (v ? 1 : 0) << (i & 31)
   return nn
 }
 export const invBit = (n: number[], i: number): ReturnType<typeof setBit> =>
